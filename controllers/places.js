16394039691e1181bebebe72ae13ee3332024434
marisol Valenzuela <marisolvalenzuela@MacBook-Air.lan1>
2023-06-05 16:12:01 -0700

    Adding home page view

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,27 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get("/new", (req, res) => {
+  res.render("places/new");
+})
+
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city){
+    req.body.city = "Anytown"
+  }
+  if (!req.body.state) {
+    req.body.state = "USA"
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    res.render('places/index', {places})
 })
+
 module.exports = router
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 require('dotenv').config()
-
 const express = require('express')
 const app = express()
 
-app.set('views engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-
-app.use('/places', require('./controllers/places')
-)
-
 app.get('/', (req, res) => {
-    res.render('Home')
+    res.render('home')
 })
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 
-app.get('*', (req,res) => {
+app.use('/places', require('./controllers/places'))
+
+
+app.get('*', (req, res) => {
     res.render('error404')
 })
 
