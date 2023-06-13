bc6d7c645433db8a2bb8c31d2dacf38a350ad59f
marisol Valenzuela <marisolvalenzuela@MacBook-Air.lan1>
2023-06-12 17:31:04 -0700

    navbar with style

--- Views/default.jsx
+++ Views/default.jsx
@@ -4,14 +4,27 @@ function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
+                <title>RestRant</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="/css/style.css"/>
             
             </head>
 
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li><a href="/places/new">Add place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
+
             </body>
         </html>
     )
--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,16 @@
 const router = require('express').Router()
-const places = require('../models/places.js')
-
+const places = require('../models/places.js');
+//const show = require('../Views/places/show.jsx');
+//index
+router.get('/', (req, res) => {
+  res.render('places/index', {places})
+   })
 //new
 router.get("/new", (req, res) => {
-  res.render("places/new");
+ res.render("places/new")
 })
+
+
 
 //post
 router.post('/', (req, res) => {
@@ -21,6 +27,16 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+   if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { places: places[id] })
+  }})
 
 module.exports = router
-
