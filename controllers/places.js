d1fa3a9b2e3f896d1fe0f7ce199c3eba98599576
marisol Valenzuela <marisolvalenzuela@MacBook-Air.local>
2023-06-19 19:25:47 -0700

    Part 8

--- controllers/places.js
+++ controllers/places.js
@@ -1,80 +1,91 @@
 const router = require('express').Router()
-const places = require('../models/places.js')
+const db = require('../models')
 
 // //index
 router.get('/', (req, res) => {
+    db.Place.find()
+    .then((places) => {
      res.render('places/index', {places})
       })
+      .catch(err => {
+        console.log(err)
+        res.render('error404')
+      })
+    })
  //new
  router.get("/new", (req, res) => {
-   res.render("places/new");
+   res.render("places/new")
  })
 
  //post
  router.post('/', (req, res) => {
-   if (!req.body.pic) {
-     req.body.pic = 'http://placekitten.com/600/600'
-   }
-   if (!req.body.city){
-     req.body.city = "Anytown"
-   }
-   if (!req.body.state) {
-     req.body.state = "USA"
-   }
-   places.push(req.body)
-   res.redirect('/places')
- })
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
 
-// //Click link/EDIT
- router.get('/:id/edit', (req, res) => {
-   let id = Number(req.params.id)
-   if (isNaN(id)) {
-       res.render('error404')
-   }
-   else if (!places[id]) {
-       res.render('error404')
-   }
-   else {
-     res.render('places/edit', {place: places[id], id})
-   }
- })
+
+//   if (!req.body.pic) {
+//     req.body.pic = 'http://placekitten.com/600/600'
+//   }
+//   if (!req.body.city){
+//     req.body.city = "Anytown"
+//   }
+//   if (!req.body.state) {
+//     req.body.state = "USA"
+//   }
+//   places.push(req.body)
+//   res.redirect('/places')
+// })
+
+
 
 // //Show/ID/Edit button
- router.put('/:id', (req, res)=>{
-   let id = Number(req.params.id)
-   if (isNaN(id)) {
-     res.render('error404')
-   }
-   else if (!places[id]) {
-     res.render('error404')
-   }
-   else {
-     if (!req.body.pic) {
-       req.body.pic = 'http://placekitten.com/600/600'
-     }     if (!req.body.city){
-       req.body.city = 'Anytown'
-     }
-     if (!req.body.state) {
-       req.body.state = 'USA'
-     }
-     places[id] = req.body
-   res.redirect(`/places/${id}`)
-   }
- })
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
 
 // //Show
- router.get('/:id', (req, res) => {
-   let id = Number(req.params.id)
-   if (isNaN(id)) {
-     res.render('error404')
-   }
-   else if (!places[id]) {
-    res.render('error404')
-   }
-   else {
-     res.render('places/show', { place: places[id], id })
-   }
- })
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+  
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//    res.render('error404')
+//   }
+//   else {
+//     res.render('places/show', { place: places[id], id })
+//   }
+// })
 
 
 // //Delete
