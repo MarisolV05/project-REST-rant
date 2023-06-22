7ebfcd356ec112ee1ce95083fa8582793a75c120
marisol Valenzuela <marisolvalenzuela@MacBook-Air.local>
2023-06-21 17:29:41 -0700

    create comment

--- Views/places/show.jsx
+++ Views/places/show.jsx
@@ -1,13 +1,13 @@
 const React = require('react')
 const Def = require('../default')
+const comment = require('../../models/comment.js')
 
 function show (data) {
-
-let comments = (
+    let comments = (
     <h3 className="inactive">
-      No comments yet!
-    </h3>
-  )
+    No comments yet!
+</h3>
+)
   if (data.place.comments.length) {
     comments = data.place.comments.map(c => {
       return (
@@ -18,60 +18,73 @@ let comments = (
             <stong>- {c.author}</stong>
           </h3>
           <h4>Rating: {c.stars}</h4>
+                  {/* <h2 className="recommend">{c.recommend ? 'Recommend! ✅' : 'Do not recommend 🚫'}</h2>  */}
         </div>
       )
     })
   }
-
-    return (
-        <Def>
-          <main>
-            <h1>{data.place.name }</h1>
-         
-         <div>
-            <h2>Rating
-                </h2>
-                <p>Not Rated</p>
-                </div> 
-                
-                <div className='row'>
-                <div className='col-sm-6'>
-                    <img src={data.place.pic} alt={data.place.name} />
-                    <h3>
-                        Located in {data.place.city}, {data.place.state}
-                    </h3>
-                </div>
-                <div className='col-sm-6'>
-                    <h2>
-                        Description
-                    </h2>
-                    <h3>
-                        {data.place.showEstablished()}
-                    </h3>
-       
+return (
+<Def>
+    <main>
+        <div className="row">
+            <div className="col-sm-6">
+                <img src={data.place.pic} alt={data.place.name} width={750} />
+                <h3>Located in {data.place.city}, {data.place.state}</h3>
+            </div>
+            <div className="col-sm-6">
+                <h1>{data.place.name}</h1>
+                <h4>Rating</h4>
+                    <h3>Not Rated</h3>
+                <br/>
+                <h2>Description</h2>
+                    <h3>Serving {data.place.cuisines}</h3>
                     <h4>
-                        Serving {data.place.cuisines}
+                        {data.place.showEstablished()}
                     </h4>
-                </div>
-                <div className='col-sm-12'>
-                    
-                    <h2>
-                        Comment
-                    </h2>
-                    {comments}
-                </div>
-                </div>
-                <a href={'/places/${data.id}/edit'} className="btn btn-warning">
-                <i className='bi bi-pencil'></i> Edit
-</a>  
-<form method="POST" action={'/places/${data.id}?_method=DELETE'}> 
-  <button type="submit" className="btn btn-danger">
-    <i className='bi bi-trash'></i>
-    Delete
-  </button>
-</form>     
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                    Edit
+                    </a> 
 
-                </main>
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                        <button type="submit" className="btn btn-danger">
+                            Delete
+                        </button>
+                    </form>     
+                <h2>Comments</h2>
+                {comments}
+            </div>
+        </div> 
+        <hr />
+    <h2>Does {data.place.name} deserve a Rave 💖 or a Rant 🤬?</h2>
+    <form action={`/places/${data.place.id}/comment`} method="POST">
+      <div className="row">
+        <div className="form-group col-sm-12">
+          <label htmlFor="content">Content</label>
+          <textarea id="content" name="content" className="form-control"></textarea>
+
+        </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+</div> 
+</div>
+                 <div className="form-group col-sm-4">
+                  <label htmlFor="stars">⭐️ Star Rating</label>
+                  <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                </div>
+                <div className="form-group col-sm-2">
+                  <label htmlFor="rant">Rant?</label>
+                  <input type="checkbox" id="rant" name="rant" className="form-control" />
+                </div>
+                {/* <div className="form-group col-sm-2">
+                  <label htmlFor="recommend">Recommend?</label>
+                  <input type="checkbox" id="recommend" name="recommend" className="form-control" />
+                </div> */}
+              </div>
+              <input type="submit" className="btn btn-primary" value="Add Comment" />
+            </form>       
+            </main>
         </Def>
     )
 }
--- controllers/places.js
+++ controllers/places.js
@@ -1,35 +1,90 @@
 const router = require('express').Router()
 const db = require('../models')
 
-// //index
 router.get('/', (req, res) => {
-    db.Place.find()
-    .then((places) => {
-     res.render('places/index', {places})
-      })
-      .catch(err => {
-        console.log(err)
-        res.render('error404')
-      })
-    })
-
- //post
- router.post('/', (req, res) => {
+  db.Place.find()
+  .then((places) => {
+    res.render('places/index', {places})
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
+  })
+})
+router.post('/', (req, res) => {
   db.Place.create(req.body)
   .then(() => {
-      res.redirect('/places')
+    res.redirect('/places')
+  })
+  .catch(err => {
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for (var field in err.errors) {
+        message += `${field} was ${err.errors[field].value}.`
+        message += `${err.errors[field].message}`
+      }
+      console.log('Validation error message', message)
+      res.render('places/new', {message})
+    } 
+    else {
+    res.render('error404')
+    }
+  })
+})
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .populate('comments')
+  .then(place => {
+    console.log(place.comments)
+    res.render('places/show', {place})
   })
   .catch(err => {
     console.log('err', err)
     res.render('error404')
-        
-
   })
 })
- //new
- router.get("/new", (req, res) => {
-  res.render("places/new")
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
 })
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+module.exports = router
 
 
 //   if (!req.body.pic) {
@@ -47,36 +102,7 @@ router.get('/', (req, res) => {
 
 
 
-// //Show/ID/Edit button
-router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
-})
 
-router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
-})
-
-router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
-})
-
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
-})
-
-// //Show
-router.get('/:id', (req, res) => {
-  db.Place.findById(req.params.id)
-  .populate('comments')
-  .then(place => {
-console.log(place.comments)
-      res.render('places/show', { place })
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
 
   
 //   let id = Number(req.params.id)
@@ -92,9 +118,3 @@ console.log(place.comments)
 // })
 
 
-// //Delete
-router.delete('/:id/rant/:rantId', (req, res) => {
-  res.send('GET /places/:id/rant/:rantId stub')
-})
-
- module.exports = router
