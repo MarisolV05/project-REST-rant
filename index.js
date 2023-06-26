d1be7d55b1266acbac066bf38a3f32354a6117b2
marisol Valenzuela <marisolvalenzuela@MacBook-Air.local>
2023-06-26 03:51:59 -0700

    New updated

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
-app.use(methodOverride('_method'))
+
 
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
   res.render('home')
 })
+const PORT = process.env.PORT || 3000
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+    () => {console.log('connected to : ', process.env.MONGO_URI)}
+    )
 
 app.get('*', (req, res) => {
   res.render('error404')
